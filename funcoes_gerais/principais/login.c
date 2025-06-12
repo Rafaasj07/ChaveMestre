4c5ac9ac07baf908c6e939b8af1991bfa58e1f69
Rafael <159143529+Rafaasj07@users.noreply.github.com>
2025-06-12 18:52:29 -0300

    Implementação do arquivo binario

--- funcoes_gerais/principais/login.c
+++ funcoes_gerais/principais/login.c
@@ -8,10 +8,10 @@
 void login(Cadastro *dados) 
 {
     char tentaiva_nome[50];
-    int existe_nome;
+    int nome_existe;
     do // Repete ate o nome do cadastro ser igual ao nome do login
     {
-        int deseja_repetir_nome, existe_senha;
+        int deseja_repetir_nome, senha_existe;
         
         limpar();
         bordas();
@@ -31,9 +31,9 @@ void login(Cadastro *dados)
         fgets(tentaiva_nome, 50, stdin);
         tentaiva_nome[strcspn(tentaiva_nome, "\n")] = '\0';
 
-        existe_nome = verificar_nome(dados, tentaiva_nome); //Caso o nome exista no arquivo, retorna 1. Senão retorna 0.
+        nome_existe = verificar_nome(dados, tentaiva_nome); //Caso o nome exista no arquivo, retorna 1. Senão retorna 0.
 
-        if (existe_nome == 1)
+        if (nome_existe == 1)
         {
             char tentaiva_senha[50];
             int aux_tentativa_senha = 0;
@@ -46,12 +46,10 @@ void login(Cadastro *dados)
                 fgets(tentaiva_senha, 50, stdin);
                 tentaiva_senha[strcspn(tentaiva_senha, "\n")] = '\0';
 
-                existe_senha = verificar_senha(dados, tentaiva_senha); //Caso a senha exista no arquivo, retorna 1. Senão retorna 0.
-
-                if (existe_senha == 1)
+                if (strcmp(dados->senha, tentaiva_senha) == 0)
                 {
                     ir_para(29, 15);
-                    printf("\033[1;32mAcesso ao programa liberado!\033[0m"); 
+                    printf("\033[1;32mAcesso ao programa liberado!\033[0m");
                 }
                 else
                 {
@@ -88,7 +86,7 @@ void login(Cadastro *dados)
                     }
                 }
 
-            } while (existe_senha == 0);
+            } while (strcmp(dados->senha, tentaiva_senha) != 0);
         }
 
         else
@@ -118,5 +116,5 @@ void login(Cadastro *dados)
                 break;
             }
         }
-    } while (existe_nome == 0);
+    } while (nome_existe == 0);
 }
--- funcoes_gerais/principais/recuperar_senha.c
+++ funcoes_gerais/principais/recuperar_senha.c
@@ -4,38 +4,71 @@
 #include "prototipos.h"
 
 // Funcao de recuperacao de senha
-void recuperar_senha(Cadastro *dados) //Lembre-se, dados é o ponteiro para a instancia la na main.
+void recuperar_senha(Cadastro *dados) // Lembre-se, dados é o ponteiro para a instancia la na main.
 {
-    char tentativa_resposta[100];
-    int continuar_tent;
+    char tentativa_resposta[100], tentativa_nome[50];
+    int continuar_tent, nome_existe;
+
+    ir_para(25, 9);
+    printf("Nome: ");
+    fgets(tentativa_nome, 50, stdin);
+    tentativa_nome[strcspn(tentativa_nome, "\n")] = '\0';
+    nome_existe = verificar_nome(dados, tentativa_nome);
+    while (nome_existe == 0)
+    {
+        int deseja_sair;
+        printf("\033[1;31mUsuario nao existe![1] Tente outro [2] Sair\033[0m \n");
+        scanf("%d", &deseja_sair);
+        apaga_buffer();
+        switch (deseja_sair)
+        {
+        case 1:
+            ir_para(25, 9);
+            printf("Nome: ");
+            fgets(tentativa_nome, 50, stdin);
+            tentativa_nome[strcspn(tentativa_nome, "\n")] = '\0';
+            nome_existe = verificar_nome(dados, tentativa_nome);
+            break;
+        case 2:
+            exit(0);
+        default:
+            limpar();
+            break;
+        }
+    }
     do
     {
-        printf("%s\n", dados->pergunta);
-        fgets(tentativa_resposta, 100, stdin);
-        tentativa_resposta[strcspn(tentativa_resposta, "\n")] = '\0';
-        if (strcmp(tentativa_resposta, dados->resposta) == 0)
+        if (nome_existe == 1)
         {
-            printf("Mude sua senha:\n");
-            fgets(dados->senha, 100, stdin);
-            dados->senha[strcspn(dados->senha, "\n")] = '\0';
-            printf("Senha altera com sucesso.\n");      
-        }
-        else
-        {
-            limpar();
-            printf("Resposta Incorreta, quer continuar tentando?\nSIM [1] SAIR [2]\n");
-            scanf("%d", &continuar_tent);
-            apaga_buffer();
-            switch (continuar_tent)
+            printf("%s\n", dados->pergunta);
+            fgets(tentativa_resposta, 100, stdin);
+            tentativa_resposta[strcspn(tentativa_resposta, "\n")] = '\0';
+            if (strcmp(tentativa_resposta, dados->resposta) == 0)
             {
-            case 1:
                 limpar();
-                break;
-            default:
-                printf("Erro ao recuperar senha.");
-                exit(0);
+                alterar_senha(dados);
+            }
+            else
+            {
+                do
+                {
+                    limpar();
+                    printf("Resposta Incorreta, quer continuar tentando?\nSIM [1] SAIR [2]\n");
+                    scanf("%d", &continuar_tent);
+                    apaga_buffer();
+                    switch (continuar_tent)
+                    {
+                    case 1:
+                        limpar();
+                        break;
+                    case 2:
+                        exit(0);
+                    default:
+                        limpar();
+                        break;
+                    }
+                } while (continuar_tent != 1 && continuar_tent != 2);
             }
         }
-
-    } while (strcmp(tentativa_resposta, dados->resposta) != 0);
+    }while (strcmp(tentativa_resposta, dados->resposta) != 0);
 }
