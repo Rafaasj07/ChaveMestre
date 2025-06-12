a0ec26b1d0a76af1bffcc0869a60e5dceaa88e66
Rafael <159143529+Rafaasj07@users.noreply.github.com>
2025-06-12 19:23:06 -0300

    Enviar email ao recuperar a senha

--- funcoes_gerais/secundarias/limpar_linha.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "prototipos.h"
-
-void limpar_linha(void){
-    printf("                                                   "); // Apaga a linha. Vai empurrando até sair do terminal
-}
